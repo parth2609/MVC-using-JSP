bae1110ecc0ccf48d0b569740b155b6547dd18a3
Parth Garg <84276178+parth2609@users.noreply.github.com>
2023-05-31 04:53:03 +0530

    Add files via upload
    Signed-off-by: Parth Garg <84276178+parth2609@users.noreply.github.com>

---
+++ ControllerServlet.java
@@ -0,0 +1,59 @@
+package com.java;  
+import java.io.IOException;  
+import java.io.PrintWriter;  
+import javax.servlet.RequestDispatcher;  
+import javax.servlet.ServletException;  
+import javax.servlet.http.HttpServlet;  
+import javax.servlet.http.HttpServletRequest;  
+import javax.servlet.http.HttpServletResponse;  
+public class ControllerServlet extends HttpServlet {  
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     *
+     * @param request
+     * @param response
+     * @throws ServletException
+     * @throws IOException
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)  
+            throws ServletException, IOException {  
+        response.setContentType("text/html");  
+        PrintWriter out=response.getWriter();  
+          
+        String name=request.getParameter("name");  
+        String password=request.getParameter("password");  
+          
+        LoginBean bean=new LoginBean();  
+        bean.setName(name);  
+        bean.setPassword(password);  
+        request.setAttribute("bean",bean);  
+          
+        boolean status=bean.validate();  
+          
+        if(status){  
+            RequestDispatcher rd=request.getRequestDispatcher("login-success.jsp");  
+            rd.forward(request, response);  
+        }  
+        else{  
+            RequestDispatcher rd=request.getRequestDispatcher("login-error.jsp");  
+            rd.forward(request, response);  
+        }  
+      
+    }  
+  
+    /**
+     *
+     * @param req
+     * @param resp
+     * @throws ServletException
+     * @throws IOException
+     */
+    @Override  
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)  
+            throws ServletException, IOException {  
+        doPost(req, resp);  
+    }  
+}  
---
+++ LoginBean.java
@@ -0,0 +1,20 @@
+package com.java;  
+public class LoginBean {  
+private String name,password;  
+  
+public String getName() {  
+    return name;  
+}  
+public void setName(String name) {  
+    this.name = name;  
+}  
+public String getPassword() {  
+    return password;  
+}  
+public void setPassword(String password) {  
+    this.password = password;  
+}  
+public boolean validate(){  
+    return password.equals("admin");  
+}  
+}  
